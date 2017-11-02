f72bfb797e724229643c39de8881cb686c79dac4
kolawole <kolawolegolulana@gmail.com>
2017-11-02 13:21:58 +0200

    Added to Firebase Succesfully

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,17 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 export const environment = {
-  production: true,
+  production: true
+  ,name: 'Production'
+  ,firebaseConfig: {
+    apiKey: "AIzaSyCkwxY5OT8OnjRts8maevQgL5nywFbyvJY",
+    authDomain: "finishing-touches.firebaseapp.com",
+    databaseURL: "https://finishing-touches.firebaseio.com",
+    projectId: "finishing-touches",
+    storageBucket: "",
+    messagingSenderId: "1072491929044"
+		/*apiKey: "YOUR_API_KEY",
+		 authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+		 databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+		 storageBucket: "YOUR_PROJECT_ID.appspot.com",
+		 messagingSenderId: "YOUR_MESSAGE_ID"*/
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,22 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
   production: false,
+  name: 'Development',
+  firebaseConfig: {
+    apiKey: "AIzaSyCkwxY5OT8OnjRts8maevQgL5nywFbyvJY",
+    authDomain: "finishing-touches.firebaseapp.com",
+    databaseURL: "https://finishing-touches.firebaseio.com",
+    projectId: "finishing-touches",
+    storageBucket: "",
+    messagingSenderId: "1072491929044"
+		/*apiKey: "YOUR_DEV_API_KEY",
+		 authDomain: "YOUR_DEV_PROJECT_ID.firebaseapp.com",
+		 databaseURL: "https://YOUR_DEV_PROJECT_ID.firebaseio.com",
+		 storageBucket: "YOUR_DEV_PROJECT_ID.appspot.com",
+		 messagingSenderId: "YOUR_DEV_MESSAGE_ID"*/
+  }
 };
